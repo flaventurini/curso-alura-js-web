eacd81c40687772f31679de981dd87c3bfda04b3
Flavia Venturini <flaviaventurini@msn.com>
2022-08-30 12:19:46 -0300

    Update

--- phone_test/main.js
+++ phone_test/main.js
@@ -1,7 +1,7 @@
 const listaDeTeclas = document.querySelectorAll('input[type=button]');
 const inputTel = document.querySelector('input[type=tel]');
 
-for (indice = 0; indice < listaDeTeclas.length; indice++) {
+for (let indice = 0; indice < listaDeTeclas.length; indice++) {
 
   const tecla = listaDeTeclas[indice];
 
